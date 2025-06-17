c85efcc867debc6c062a2d758fff2dbdf6ffc7a8
LeCodeur237 <erastedzou@gmail.com>
2025-06-17 18:32:26 +0200

    login

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 
 // Email Verification Route (must be publicly accessible but signed)
